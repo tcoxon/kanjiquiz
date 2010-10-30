c9e0f776dee73f722dbb335ef979bfb6de059961
Tom Coxon <tom.c.coxon@googlemail.com>
2010-10-30 23:10:50 +0100

    Add perldoc.

--- quiz.pl
+++ quiz.pl
@@ -28,14 +28,12 @@ my %normalizers = (
 );
 
 my %commands = (
-    help => [\&cmd_help, 'List commands and their descriptions'],
-    quit => [\&cmd_quit, 'Leave Kanjiquiz'],
-    load => [\&cmd_load, '1 Argument - Load a Kanji database'],
-    grade => [\&cmd_grade, '1 Argument - Load a Kanji database of a particular Kyouiku '.
-        'grade'],
-    train => [\&cmd_train, 'Present Kanji one at a time with their definitions'.
-        ' and readings and quiz you on them immediately after seeing them'],
-    quiz => [\&cmd_quiz, 'Quiz you on Kanji meanings and readings'],
+    help => \&cmd_help,
+    quit => \&cmd_quit,
+    load => \&cmd_load,
+    grade => \&cmd_grade,
+    train => \&cmd_train,
+    quiz => \&cmd_quiz,
 );
 
 # definitions of things commands are allowed to change
@@ -52,18 +50,10 @@ my $term_height;
 
 # Commands:
 
-sub show_universal_commands {
-    print "Enter ':quit' (or send EOF) at any prompt to exit.",
-        " During training or a quiz, you may also enter ':cancel' to get back",
-        " to the main prompt.\n";
-}
-
 sub cmd_help {
-    for (keys %commands) {
-        print "$_ - $commands{$_}->[1]\n";
-    }
-    print "\n";
-    show_universal_commands();
+    my $cmd = 'perldoc "'.__FILE__.'"';
+    print "[$cmd]\n";
+    system($cmd) and die "Perldoc failed with status $?";
 }
 
 sub cmd_quit {
@@ -276,7 +266,9 @@ sub main {
     setup();
 
     print "Kanjiquiz\n";
-    show_universal_commands();
+    print "Enter ':quit' (or send EOF) at any prompt to exit.",
+        " During training or a quiz, you may also enter ':cancel' to get back",
+        " to the main prompt.\n";
     print "\nThis Kanji tutor is interactive. Enter 'help' for a list of ",
         "valid commands.\n\n";
 
@@ -289,7 +281,7 @@ sub main {
 
             if (defined $commands{$command}) {
                 eval {
-                    $commands{$command}->[0]->($args);
+                    $commands{$command}->($args);
                 };
                 if ($@ !~ /^:quit/) {
                     print STDERR $@, "\n";
@@ -311,3 +303,169 @@ sub main {
 }
 
 main();
+
+__END__
+
+=encoding utf-8
+
+=head1 KANJIQUIZ
+
+Kanjiquiz - Kanji tutor and quizmaster
+
+=head1 SYNOPSIS
+
+    perl quiz.pl
+
+=head1 DESCRIPTION
+
+Kanjiquiz uses a database of kanji to present and test you on their
+meanings and readings.
+
+On starting quiz.pl, you'll be presented with a command prompt. The
+following section documents the available commands.
+
+=head2 COMMANDS
+
+=over
+
+=item help
+
+Displays this documentation.
+
+=item quit
+
+Exits Kanjiquiz.
+
+=item load I<file>
+
+Replaces the current database with the kanji in the specified file.
+
+=item grade I<grade>
+
+Replaces the current database with the list of kanji learned in kyouiku grade I<grade>. This is the same as entering "load kyouiku/I<grade>.csv".
+
+=item train
+
+Training mode. This will go through the list of kanji one by one. For each
+kanji, it will display the data for you to memorize. Once you have memorized the
+data, it will clear the screen and ask you to repeat it back.
+
+Input for meanings must be in English.
+
+Input for readings may be in romaji or kana, but if you enter answers in kana,
+it must be in the correct kana (i.e. Katakana for On'yomi, and Hiragana for 
+Kun'yomi).
+
+=item quiz
+
+Quiz mode. This will go through the list of kanji one by one and ask you to
+remember meanings and readings for each one, without first showing you what
+they are.
+
+Input for meanings must be in English.
+
+Input for readings may be in romaji or kana, but if you enter answers in kana,
+it must be in the correct kana (i.e. Katakana for On'yomi, and Hiragana for
+Kun'yomi).
+
+=back
+
+=head2 SPECIAL COMMANDS
+
+The following special commands may be entered at any prompt (not just the main
+prompt).
+
+=over
+
+=item :quit
+
+Quits Kanjiquiz completely. Sending EOF (Ctrl-D on some systems) will have the
+same effect.
+
+=item :cancel
+
+Entering this during a quiz or during training will cause the current training
+or quiz to be canceled, leaving you at the main prompt.
+
+=back
+
+=head2 CUSTOMIZATION
+
+=head3 Writing your own databases
+
+Kanjiquiz allows you to specify your own kanji databases if you would like to
+learn a particular subset of kanji that doesn't fall into one of the kyouiku
+grades. These database can be loaded using the C<load> command.
+
+The databases are UTF-8 CSV files. The columns are:
+
+=over
+
+=item * Kanji
+
+=item * Meanings
+
+=item * On'yomi
+
+=item * Kun'yomi
+
+=back
+
+All the columns apart from the first allow you to specify multiple entries by
+separating values with the ASCII vertical bar (C<|>) character.
+
+Readings should be written in romaji. Where necessary, separate distinct mora /
+syllables using the ASCII hyphen (C<->) character.
+
+Inspection of the Kyouiku grade databases (kyouiku/*.csv) should aid in
+understanding the format. Example rows:
+
+    一,one,ichi|itsu,hito-tsu
+    月,month|moon,gatsu|getsu,tsuki
+
+=head1 REQUIREMENTS
+
+This software requires the following on your machine:
+
+=over
+
+=item Term::ReadLine
+
+You can get this with CPAN if it's not already installed on your computer.
+Or, on Ubuntu, you can install it with:
+
+    apt-get install libterm-readline-gnu-perl
+
+=item UTF-8-capable terminal
+
+Output and input to the program is in the UTF-8 character set, so you will
+need a terminal capable of accepting and displaying these characters.
+
+=item stty
+
+To determine the height of your terminal, quiz.pl calls out to the stty
+command. If C<stty> is not on your PATH, you may instead specify the height of
+your terminal in the environment variable C<TERM_HEIGHT>.
+
+=back
+
+=head1 AUTHORS
+
+Tom Coxon and Jacob C Kesinger.
+
+Kyouiku database files (kyouiku/*.csv) were created using the Wikipedia
+page on Kyouiku Kanji:
+L<http://en.wikipedia.org/wiki/Ky%C5%8Diku_kanji>
+
+=head1 COPYRIGHT AND LICENSE
+
+Kanjiquiz is copyright (C) 2010, Tom Coxon. Lingua::JA::Romaji is copyright of
+Jacob C Kesinger. Both are distributed under the GNU General Public License
+version 2.
+
+The Kyouiku Kanji database is licensed under the Create Commons
+Attribution-ShareAlike license.
+
+=head1 SEE ALSO
+
+L<http://github.com/tcoxon/kanjiquiz/>
